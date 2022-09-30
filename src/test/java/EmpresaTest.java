fc770cbb33ecfe9fcb02ba16d521c0c292d657fa
IvanylsonHG <ivanylson.honorio.goncalves@ice.ufjf.br>
2022-09-29 22:32:15 -0300

    Adiciona a classe de teste

--- src/test/java/EmpresaTest.java
+++ src/test/java/EmpresaTest.java
@@ -1,5 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmpresaTest {
+    @Test
+    void deveRetornarNomeDaEmpresa()
+    {
+        Empresa.getInstance().setNomeEmpresa("Unidos não venceremos");
+        assertEquals("Unidos não venceremos", Empresa.getInstance().getNomeEmpresa());
+    }
+    @Test
+    void deveRetornarNomeDoDiretor()
+    {
+        Empresa.getInstance().setNomeDiretor("Birobiro");
+        assertEquals("Birobiro", Empresa.getInstance().getNomeDiretor());
+    }
 
 }
